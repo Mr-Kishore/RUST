3c6f34dc802d094f395d3392adca01e84bd4a4cb
Kishore <kishoreprogramminghub@gmail.com>
2025-06-19 00:15:30 +0530

    source files

---
+++ src/level1.rs
@@ -0,0 +1,11 @@
+// rust-learning/src/level1.rs
+
+pub fn run() {
+    let name = "Kishore";
+    let level = 1;
+    let health = 100;
+
+    println!("Player: {}", name);
+    println!("Level: {}", level);
+    println!("Health: {}", health);
+}
---
+++ src/main.rs
@@ -0,0 +1,7 @@
+mod level1; // this links to your level1.rs file
+
+fn main() {
+    println!(" Rust Learning Begins...\n");
+
+    level1::run(); // calling the level1 run function
+}
